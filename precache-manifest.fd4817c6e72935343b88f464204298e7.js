e539213cd41671e8900df4f43fce53367d2c830b
Arpit Bansal <arpit.bansal@outlook.com>
2020-10-24 21:34:06 +0530

    Updates

---
+++ precache-manifest.fd4817c6e72935343b88f464204298e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ac0ecce4d1ace07d692b8c49d86c35",
+    "url": "/admin-dashboard/index.html"
+  },
+  {
+    "revision": "ecd5fa0f79d3724b7347",
+    "url": "/admin-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ce3aef2ceb2b2b64ff13",
+    "url": "/admin-dashboard/static/js/2.4efce7e6.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/admin-dashboard/static/js/2.4efce7e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd5fa0f79d3724b7347",
+    "url": "/admin-dashboard/static/js/main.a5396047.chunk.js"
+  },
+  {
+    "revision": "db6bc4bf7a0b27b22411",
+    "url": "/admin-dashboard/static/js/runtime-main.af49b450.js"
+  }
+]);
